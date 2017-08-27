9f360b50a2775e70b64847c95acaa615d123bca2
tokenether <adwikelove@gmail.com>
2017-08-27 19:21:20 +0000

    TokenEther

---
+++ config_testnet.js
@@ -0,0 +1,37 @@
+/* eslint-env browser */
+
+module.exports = {
+  homeURL: 'http://localhost:8080',
+  contractTokenEther: 'smart_contract/tokenether.sol',
+  contractToken: 'smart_contract/token.sol',
+  contractReserveToken: 'smart_contract/reservetoken.sol',
+  contractTokenEtherAddrs: [
+    { addr: '0xbca13cbebff557143e8ad089192380e9c9a58c70', info: 'Deployed 08/10/2017' },
+    
+  ],
+  ethTestnet: 'ropsten',
+  ethProvider: 'http://localhost:8545',
+  ethGasPrice: 20000000000,
+  ethAddr: '0x0000000000000000000000000000000000000000',
+  ethAddrPrivateKey: '',
+  gasApprove: 250000,
+  gasDeposit: 250000,
+  gasWithdraw: 250000,
+  gasTrade: 250000,
+  gasOrder: 250000,
+  ordersOnchain: false,
+  apiServer: 'http://localhost:3000',
+  userCookie: 'TokenEther',
+  eventsCacheCookie: 'TokenEther_eventsCache',
+  deadOrdersCacheCookie: 'TokenEther_deadOrdersCache',
+  ordersCacheCookie: 'TokenEther_ordersCache',
+  etherscanAPIKey: 'SCYVG55I4EYS4JJ82NYMV87MGDGVNNZJ49',
+  tokens: [
+    { addr: '0x0000000000000000000000000000000000000000', name: 'ETH', decimals: 18 },
+    { addr: '0x40aade55175aaeed9c88612c3ed2ff91d8943964', name: '1ST', decimals: 18 },
+  ],
+  defaultPair: { token: '1ST', base: 'ETH' },
+  pairs: [
+    { token: '1ST', base: 'ETH' },
+  ],
+};
